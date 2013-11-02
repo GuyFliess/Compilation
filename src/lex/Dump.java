ec7082a90ffe4fc569a434a3d9e358fca5c2766d
litalyod <litalyod@gmail.com>
2013-11-02 15:16:15 +0200

    Update Dump.java

--- src/lex/Dump.java
+++ src/lex/Dump.java
@@ -46,14 +46,14 @@ public class Dump
 	
 	public void output() {
 		String dump, tag, token, line, column;
-		System.out.format("%-13s%-13s%-5s%-10s\n", "token", "tag", "line", ": column");
+		System.out.format("%-13s%-13s%-5s%-8s\n", "token", "tag", "line", ": column");
 		for (Token tok: matches) {
 			tag = tok.tag;
 			token = tok.value;
 			line = Integer.toString(tok.line);
 			column = Integer.toString(tok.column);
 			if (tag=="ERROR") {
-				System.out.format("%s:%s : lexical error; %s \n", line, column, token);
+				System.out.format("%s:%s : lexical error; %s\n", line, column, token);
 				break;
 			}
 			System.out.format("%-12s %-11s%4s%8s\n", token, tag, line, column);
