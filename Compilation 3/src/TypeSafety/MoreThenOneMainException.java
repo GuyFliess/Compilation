c417cfe753ffeb8496daa67037852dfeb7160b9b
litalyod <litalyod@gmail.com>
2013-12-03 14:30:10 +0200

    main..

---
+++ Compilation 3/src/TypeSafety/MoreThenOneMainException.java
@@ -0,0 +1,9 @@
+package TypeSafety;
+
+public class MoreThenOneMainException extends TypeSafetyException{
+
+	public MoreThenOneMainException(String message, int line) {
+		super(message, line);
+	}
+	
+}
