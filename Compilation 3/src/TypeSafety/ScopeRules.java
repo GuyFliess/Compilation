90642d2a36a6667758ccf5127d8022e5e72c9e60
litalyod <litalyod@gmail.com>
2013-12-04 13:45:17 +0200

    check multiple declaration - still not working...

--- Compilation 3/src/TypeSafety/ScopeRules.java
+++ Compilation 3/src/TypeSafety/ScopeRules.java
@@ -1,6 +1,20 @@
 package TypeSafety;
 
+import ic.ast.decl.Program;
+
 public class ScopeRules {
+	
+	public void CheckScopeRules(Program p) {
+		
+		CheckMultipleDefinition mulDef = new CheckMultipleDefinition();
+		
+		try {
+			mulDef.CheckDef(p);
+		} catch (MultipleDefineException e) {
+			System.err.println(e.lineNum+": semantic error; Id "+e.errorMSG+" already defined in current scope");
+		}
+		
+	}
 	
 	
 	
